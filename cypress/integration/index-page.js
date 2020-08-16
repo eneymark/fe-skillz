de78338c2d9f42b03aadae61f6c6ceb4619bc6da
eneymark <eneymark@yahoo.com>
2020-08-16 19:29:01 -0400

    adding in more cypress tests

--- cypress/integration/index-page.js
+++ cypress/integration/index-page.js
@@ -1,3 +1,5 @@
+import navData from '../../data/navbar.json';
+
 describe("Index page", () => {
     /*
     * Visits the page before each test
@@ -8,6 +10,19 @@ describe("Index page", () => {
     });
 
     it("should have a logo", () => {
-        cy.get(".main").should("have.length", 1);
+        cy.get("[data-test=logo]").should("have.length", 1);
+    });
+
+    it('should have a navbar', ()=>{
+        cy.get("[data-test=navbar]").should("have.length", 1);
+    });
+
+    it('should open submenu items', ()=>{
+       navData.navItems.map(navItem=>{
+           if(navItem.subMenu){
+               cy.get(`[data-test='item:${navItem.title}']`).click()
+                   .find("[data-test=submenu]").should("have.length", navItem.subMenu.length);
+           }
+       });
     });
 });
